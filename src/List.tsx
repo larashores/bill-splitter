d412331e4deb4e4125318038b7d936c7dd995a00
Lara Shores <lara.shores@outlook.com>
2023-06-26 17:43:35 -0700

    List and Toggle modifications

--- src/List.tsx
+++ src/List.tsx
@@ -1,17 +1,20 @@
 import React from 'react'
 import './App.css'
 
+type ColumnSpecifier = {name: string, type?: string}
 
-function List() {
-    const [items, setItems] = React.useState([{amount: "", name: ""}])
+function List(props: {columns: Array<ColumnSpecifier>}) {
+    const empty = props.columns.reduce((acc, item) => ({ ...acc, [item.name]: "" }), {})
+    const [items, setItems] = React.useState([empty])
 
     function onChange(event: {target: {value: string}}, ind: number, field: string) {
-        const newItems = items.map((item, i) => i === ind ? {...item, [field]: event.target.value} : item).filter(
-            (item, i) => item.amount || item.name || ind == i
+        const newItems = items.map(
+            (item, i) => i === ind ? {...item, [field]: event.target.value} : item
+        ).filter(
+            (item, i) => Object.values(item).some(x => x) || ind == i
         )
-        
-        if (newItems.every((item) => item.amount || item.name)) {
-            newItems.push({amount: "", name: ""})
+        if (newItems.every((item) => Object.values(item).some(x => x))) {
+            newItems.push(empty)
         }
         setItems(newItems)
         console.log(newItems)
@@ -21,14 +24,25 @@ function List() {
     <>
         <ul>
             <li key="header">
-                <label>Name</label>
-                <label>Amount</label>
+                {
+                    props.columns.map(
+                        (item) => <label>{item.name}</label>
+                    )
+                }
             </li>
             {
                 items.map(
                     (item, ind) => <li key={ind}>
-                        <input onChange={(e) => onChange(e, ind, "name")} value={item.name}></input>
-                        <input onChange={(e) => onChange(e, ind, "amount")} value={item.amount} type="number"></input>
+                        {
+                            props.columns.map(
+                                (col) => col.type == "select" ? 
+                                <select onChange={(e) =>  onChange(e, ind, col.name)} value={item[col.name]}>
+                                    <option value="Lara">Lara</option>
+                                    <option value="Kat">Kat</option>
+                                </select>: 
+                                <input onChange={(e) => onChange(e, ind, col.name)} value={item[col.name]} type={col.type}/>
+                            )
+                        }
                     </li>
                 )
             }
--- src/Toggle.tsx
+++ src/Toggle.tsx
@@ -2,16 +2,22 @@ import React from 'react'
 import "./Toggle.css"
 
 
-function Toggle(props: any) {
+function Toggle(props: {id?: string}) {
+    
+    const id = props.id || "toggle-" + Toggle.nextID++
+
+    const [checked, setChecked] = React.useState(true)
+
     return <>
-        <div className="toggle-switch">
-            <input type="checkbox" className="toggle-switch-checkbox" name="toggleSwitch" id="toggleSwitch" />
-            <label className="toggle-switch-label" htmlFor="toggleSwitch">
-                <span className="toggle-switch-inner"/>
-                <span className="toggle-switch-switch"/>
+        <div className="toggle">
+            <input type="checkbox" className="toggle-checkbox" name="toggleSwitch" id={id} checked={checked} onChange={e => setChecked(e.target.checked)}/>
+            <label className="toggle-label" htmlFor={id}>
+                <span className="toggle-inner"/>
+                <span className="toggle-switch"/>
             </label>
         </div>
     </>
 }
+Toggle.nextID = 0
 
 export default Toggle
