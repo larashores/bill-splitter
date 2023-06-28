8008aee70d586ade371dbc1cad5165db322e7b11
Lara Shores <lara.shores@outlook.com>
2023-06-27 19:27:21 -0700

    Add user addable fees and fee types

--- src/App.tsx
+++ src/App.tsx
@@ -5,20 +5,12 @@ import './App.css'
 
 
 function App() {
-    const [items, setItems] = React.useState([{amount: "", name: "", person: ""}])
     const [people, setPeople] = React.useState([""])
-    const [tax, setTax] = React.useState(0)
-    const [tip, setTip] = React.useState(0)
+    const [items, setItems] = React.useState([{amount: "", name: "", person: ""}])
+    const [fees, setFees] = React.useState([{amount: "", name: "Tax", type: "flat"}, {amount: "", name: "Tip", type: "flat"}, {amount: "", name: "", type: "flat"}])
 
-    function onItemChange(event: {target: {value: string}}, ind: number, field: string) {
-        const newItems = items.map((item, i) => i === ind ? {...item, [field]: event.target.value} : item).filter(
-            (item, i) => item.amount || item.name || ind == i
-        )
-        
-        if (newItems.every((item) => item.amount || item.name)) {
-            newItems.push({amount: "", name: "", person: ""})
-        }
-        setItems(newItems)
+    function prefix(pre: string, value: string, ): string {
+        return value ? pre + value : value
     }
 
     function onPersonChange(event: {target: {value: string}}, ind: number) {
@@ -34,17 +26,29 @@ function App() {
 
     }
 
-    function getTaxPercent(): number {
-        const total = items.filter(
-            (row) => row.amount
-        ).map(
-            (row) => Number(row.amount)
-        ).reduce(
-            (amount, current) => Number(amount) + current, 0
+    function onItemChange(event: {target: {value: string}}, ind: number, field: string) {
+        const newItems = items.map((item, i) => i === ind ? {...item, [field]: event.target.value} : item).filter(
+            (item, i) => item.amount || item.name || ind == i
         )
-        return tax / total
+        
+        if (newItems.every((item) => item.amount || item.name)) {
+            newItems.push({amount: "", name: "", person: ""})
+        }
+        setItems(newItems)
     }
-    function getTipPercent(): number {
+
+    function onFeeChange(event: {target: {value: string}}, ind: number, field: string) {
+        const newFees = fees.map((item, i) => i === ind ? {...item, [field]: event.target.value} : item).filter(
+            (item, i) => item.amount || item.name || ind == i
+        )
+        
+        if (newFees.every((row) => row.amount || row.name)) {
+            newFees.push({amount: "", name: "", type: "flat"})
+        }
+        setFees(newFees)
+    }
+
+    function breakdownFees(value: number) {
         const total = items.filter(
             (row) => row.amount
         ).map(
@@ -52,7 +56,12 @@ function App() {
         ).reduce(
             (amount, current) => Number(amount) + current, 0
         )
-        return tip / total
+        const amounts = fees.filter(
+            (row) => row.amount && row.name
+        ).map(
+            (row) => ({amount: (row.type == "flat" ? Number(row.amount) / total : Number(row.amount) / 100) * value, name: row.name})
+        )
+        return amounts
     }
 
     function getTotal(person: string): number {
@@ -63,19 +72,13 @@ function App() {
         ).reduce(
             (amount, current) => Number(amount) + current, 0
         )
-        const total = items.filter(
+        const total = breakdownFees(subtotal).map(
             (row) => row.amount
-        ).map(
-            (row) => Number(row.amount)
         ).reduce(
-            (amount, current) => Number(amount) + current, 0
+            (amount, current) => amount + current, subtotal
         )
-        const tax_percent = tax / total
-        const tip_percent = tip / total
-        const result = subtotal * (1 + tax_percent + tip_percent)
-        return Number.isNaN(result) ? 0 : result
+        return Number.isNaN(total) ? 0 : total
     }
-
 
     return (
     <>
@@ -113,18 +116,21 @@ function App() {
         </div>
         <h2>Fees</h2>
         <div id="layout">
-            <label>Tax:</label>
-            <input type="number" value={tax} onChange={(e) => setTax(e.target.value)}></input>
-            <select value="flat">
-                <option value="Flat">Flat</option>
-                <option value="Percent">Percent</option>
-            </select>
-            <label>Tip:</label>
-            <input type="number" value={tip} onChange={(e) => setTip(e.target.value)}></input>
-            <select value="flat">
-                <option value="Flat">Flat</option>
-                <option value="Percent">Percent</option>
-            </select>
+            <label>Name</label>
+            <label>Amount</label>
+            <label>Type</label>
+            {
+                fees.map(
+                    (row, ind) => <>
+                        <input onChange={(e) => onFeeChange(e, ind, "name")} value={row.name}/>
+                        <input onChange={(e) => onFeeChange(e, ind, "amount")} value={row.amount} type="number"/>
+                        <select value={row.type} onChange={(e) => onFeeChange(e, ind, "type")}>
+                            <option value="Flat">Flat</option>
+                            <option value="Percent">Percent</option>
+                        </select>
+                    </>
+                )
+            }
         </div>
         <h2>Results</h2>
         <ul>
@@ -137,7 +143,21 @@ function App() {
                             {
                                 items.filter((row) => row.person == person && row.amount).map(
                                     (row, ind) => <li key={ind}>
-                                        {row.name} ${(row.amount * (1 + getTaxPercent() + getTipPercent())).toFixed(2)} = ${row.amount} + ${(Number(row.amount) * getTaxPercent()).toFixed(2)} Tax + ${(Number(row.amount) * getTipPercent()).toFixed(2)} Tip
+                                        {row.name} : ${
+                                            breakdownFees(Number(row.amount)).map(
+                                                (row) => row.amount
+                                            ).reduce(
+                                                (amount, current) => amount + current,
+                                                Number(row.amount)
+                                            ).toFixed(2)
+                                        }{
+                                            prefix(
+                                                ` = $${row.amount} + `,
+                                                breakdownFees(Number(row.amount)).map(
+                                                    (row) => `$${row.amount.toFixed(2)} ${row.name}`
+                                                ).join(" + "),
+                                            )
+                                        }
                                     </li>
                                 )
                             }
