8a317268dda6625f58b5b5a9d0d2a44b2fe21396
Lara Shores <lara.shores@outlook.com>
2023-07-25 19:20:25 -0700

    Update List

--- src/List.tsx
+++ src/List.tsx
@@ -1,54 +1,58 @@
-// import React from 'react'
-// import './App.css'
+import React from "react";
+import "./App.css";
 
-// type ColumnSpecifier = {name: string, type?: string}
+type Event = { target: { value: string } };
 
-// function List(props: {columns: Array<ColumnSpecifier>}) {
-//     const empty = props.columns.reduce((acc, item) => ({ ...acc, [item.name]: "" }), {})
-//     const [items, setItems] = React.useState([empty])
+type ColumnSpecifier = {
+    name: string,
+    Type: (
+        onChange: (event: Event) => void,
+        value: string,
+    ) => any
+};
 
-//     function onChange(event: {target: {value: string}}, ind: number, field: string) {
-//         const newItems = items.map(
-//             (item, i) => i === ind ? {...item, [field]: event.target.value} : item
-//         ).filter(
-//             (item, i) => Object.values(item).some(x => x) || ind == i
-//         )
-//         if (newItems.every((item) => Object.values(item).some(x => x))) {
-//             newItems.push(empty)
-//         }
-//         setItems(newItems)
-//         console.log(newItems)
-//     }
+function List(props: { columns: Array<ColumnSpecifier> }) {
+  const empty = props.columns.reduce(
+    (acc, item) => ({ ...acc, [item.name]: "" }),
+    {}
+  );
+  const [items, setItems] = React.useState([empty]);
 
-//     return (
-//     <>
-//         <ul>
-//             <li key="header">
-//                 {
-//                     props.columns.map(
-//                         (item) => <label>{item.name}</label>
-//                     )
-//                 }
-//             </li>
-//             {
-//                 items.map(
-//                     (item, ind) => <li key={ind}>
-//                         {
-//                             props.columns.map(
-//                                 (col) => col.type == "select" ?
-//                                 <select onChange={(e) =>  onChange(e, ind, col.name)} value={item[col.name]}>
-//                                     <option value="Lara">Lara</option>
-//                                     <option value="Kat">Kat</option>
-//                                 </select>:
-//                                 <input onChange={(e) => onChange(e, ind, col.name)} value={item[col.name]} type={col.type}/>
-//                             )
-//                         }
-//                     </li>
-//                 )
-//             }
-//         </ul>
-//     </>
-//     )
-// }
+  function onChange2(event: Event, ind: number, field: string) {
+    const newItems = items
+      .map((item, i) =>
+        i === ind ? { ...item, [field]: event.target.value } : item
+      )
+      .filter((item, i) => Object.values(item).some((x) => x) || ind == i);
+    if (newItems.every((item) => Object.values(item).some((x) => x))) {
+      newItems.push(empty);
+    }
+    setItems(newItems);
+    console.log(newItems);
+  }
 
-// export default List
+  return (
+    <>
+      <table>
+        <tr key="header">
+          {props.columns.map((item) => (
+            <th>
+              <label>{item.name}</label>
+            </th>
+          ))}
+        </tr>
+        {items.map((item, ind) => (
+          <tr key={ind}>
+            {props.columns.map((col) => (
+              <td>
+                <col.Type onChange={(e) => onChange2(e, ind, col.name)} value={item[col.name]}></col.Type>
+              </td>
+            ))}
+          </tr>
+        ))}
+      </table>
+    </>
+  );
+}
+
+export default List;
